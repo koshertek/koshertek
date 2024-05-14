d7f195ccbe45c5a05eb918e70fd5974228be8e82
koshertek <138523128+koshertek@users.noreply.github.com>
2024-05-14 15:01:10 -0400

    Add files via upload

---
+++ 4058R/flash.bat
@@ -0,0 +1,206 @@
+@echo off
+color
+
+setlocal
+
+
+rem Read the contents of %koshertek%\location into the location variable
+set "location="
+for /f "usebackq delims=" %%a in ("%koshertek%\location") do set "location=%%a"
+
+rem Read the contents of %koshertek%\username into the username variable
+set "username="
+for /f "usebackq delims=" %%a in ("%koshertek%\username") do set "username=%%a"
+
+rem Check if the fileschecked flag exists
+if not exist "%koshertek%\4058R\fileschecked" (
+    "%koshertek%\4058R\checksize.exe"
+
+    rem Check the errorlevel and act accordingly
+    if errorlevel 1 (
+        echo Files did not download correctly! Contact KosherTek for help.
+        pause >nul 2>&1
+        exit /b 1
+    ) else (
+        echo. > "%koshertek%\4058R\fileschecked"
+    )
+)
+:choices:
+cls
+echo ##############################################
+echo              KosherTek Flash Tool             
+echo ##############################################
+echo.
+echo Selected phone: TCL 4058R
+echo.
+echo Tag Location: %location%
+echo.
+echo Username: %username%
+echo.
+echo Select a profile to install:
+echo.
+echo [31mFor a reflash enter ref with no numbers.[0m 
+echo.
+echo 1. Talk, Text, Waze, Voice2Text
+echo.
+echo 2. Talk, Text, Waze, Voice2Text, Smartlist
+echo.
+echo 3. Talk, Text, Waze, Voice2Text, Email
+echo.
+echo 4. Talk, Text, Waze, Voice2Text, Smartlist, Email
+echo.
+set /p choice=Enter the profile number and press enter: 
+
+if "%choice%"=="1" (
+    set "boot=%koshertek%\4058R\noenos.img"
+	set "prof=Talk, Text, Waze, Voice2Text"
+) else if "%choice%"=="2" (
+    set "boot=%koshertek%\4058R\noemail.img"
+	set "prof=Talk, Text, Waze, Voice2Text, Smartlist"
+) else if "%choice%"=="3" (
+    set "boot=%koshertek%\4058R\yesemailnos.img"
+	set "prof=Talk, Text, Waze, Voice2Text, Email"
+) else if "%choice%"=="4" (
+    set "boot=%koshertek%\4058R\yesemail.img"
+	set "prof=Talk, Text, Waze, Voice2Text, Smartlist, Email"
+) else if "%choice%"=="ref" (
+    "%koshertek%\4058R\ref.bat"
+	goto exit
+) else (
+	cls
+	echo ##############################################
+	echo              KosherTek Flash Tool             
+	echo ##############################################
+	echo.
+    echo [31mInvalid Choice. Please try again.[0m
+	echo.
+    goto choices
+)
+
+
+
+
+
+
+
+cls
+echo ##############################################
+echo              KosherTek Flash Tool             
+echo ##############################################
+
+echo.
+echo Tag Location: %location%
+echo.
+
+echo Username: %username%
+echo.
+
+echo.
+echo Selected phone: TCL 4058R
+echo.
+echo.
+echo Selected Profile: %prof%
+echo.
+
+REM echo [31mPLEASE NOTE: The installation for this model TCL is different than others.[0m
+REM echo [31mThe phone will enter a few different modes. DO NOT touch the phone until the tool says it is done!![0m
+REM echo [31mDO NOT press[0m[32m "REBOOT SYSTEM" [0m[31mon the phone.[0m
+REM echo.
+
+
+
+
+
+
+start /B "" "%koshertek%\adb\autobooter.exe" >nul 2>&1
+echo.
+echo Please inform the customer that the phone will be reset!
+echo.
+echo Connect phone while it is powered off. If the phone goes to charging mode just disconnect and reconnect it.
+echo.
+echo Press volume up when prompted by the phone
+"%koshertek%\adb\fastboot.exe" flashing unlock >nul 2>&1
+TASKKILL /F /IM autobooter.exe >nul 2>&1
+cls
+echo ##############################################
+echo              KosherTek Flash Tool             
+echo ##############################################
+echo.
+echo Flashing phone now. Do not disconnect the phone until told!
+"%koshertek%\adb\fastboot.exe" --disable-verity --disable-verification flash boot "%boot%" >nul 2>&1
+"%koshertek%\adb\fastboot.exe" --disable-verity --disable-verification flash vbmeta "%koshertek%\4058R\emptyvbmeta" >nul 2>&1
+"%koshertek%\adb\fastboot.exe" --disable-verity --disable-verification flash vbmeta_system "%koshertek%\4058R\emptyvbmeta" >nul 2>&1
+"%koshertek%\adb\fastboot.exe" --disable-verity --disable-verification flash vbmeta_vendor "%koshertek%\4058R\emptyvbmeta" >nul 2>&1
+"%koshertek%\adb\fastboot.exe" flash lk "%koshertek%\4058R\lk.img" >nul 2>&1
+"%koshertek%\adb\fastboot.exe" flash md1img "%koshertek%\4058R\md1img.img" >nul 2>&1
+"%koshertek%\adb\fastboot.exe" flash tee1 "%koshertek%\4058R\tee.img" >nul 2>&1
+"%koshertek%\adb\fastboot.exe" flash dtbo "%koshertek%\4058R\dtbo.img" >nul 2>&1
+"%koshertek%\adb\fastboot.exe" flash loader_ext1 "%koshertek%\4058R\loader_ext.img" >nul 2>&1
+"%koshertek%\adb\fastboot.exe" flash mcupmfw "%koshertek%\4058R\mcupmfw.img" >nul 2>&1
+"%koshertek%\adb\fastboot.exe" flash spmfw "%koshertek%\4058R\spmfw.img" >nul 2>&1
+"%koshertek%\adb\fastboot.exe" flash logo "%koshertek%\4058R\logo.bin" >nul 2>&1
+"%koshertek%\adb\fastboot.exe" flash preloader "%koshertek%\4058R\preloader_raw.img" >nul 2>&1
+"%koshertek%\adb\fastboot.exe" flash md1dsp "%koshertek%\4058R\md1dsp.img" >nul 2>&1
+"%koshertek%\adb\fastboot.exe" --disable-verity --disable-verification flash pgpt "%koshertek%\4058R\pgpt" >nul 2>&1
+TASKKILL /F /IM autobooter.exe >nul 2>&1
+start /B "" "%koshertek%\adb\autobooter.exe" >nul 2>&1
+:atbtr:
+tasklist | findstr /i autobooter.exe >nul 2>&1
+if errorlevel 1 (
+    
+    goto atbtr
+) else (
+    
+	"%koshertek%\adb\fastboot.exe" reboot >nul 2>&1
+)
+"%koshertek%\adb\fastboot.exe" --disable-verity --disable-verification flash recovery "%koshertek%\4058R\recovery3.bin" >nul 2>&1
+
+"%koshertek%\adb\fastboot.exe" reboot fastboot >nul 2>&1
+TASKKILL /F /IM autobooter.exe >nul 2>&1
+rem Part 3 
+"%koshertek%\4058R\superflash" "%koshertek%\adb\fastboot.exe" flash super "%koshertek%\4058R\4058Rsuper.img"
+
+cls
+color 4f
+echo ##############################################
+echo              KosherTek Flash Tool             
+echo ##############################################
+echo.
+curl --insecure "https://script.google.com/macros/s/AKfycbyAD9xhik0W_dVRrlWTqVgCYHb8hqK1Hg6q55yAC3I_cLLAo4yPjAfW9XqBYwEdG0Zq/exec" --data-urlencode "param1=%location%" --data-urlencode "param2=%username%" --data-urlencode "param3=4058R" --data-urlencode "param4=%prof%" >nul 2>&1
+
+echo Do not disconnect phone. One last thing...
+echo.
+
+TASKKILL /F /IM autobooter.exe >nul 2>&1
+start /B "" "%koshertek%\adb\autobooter.exe" >nul 2>&1
+:atbtr2:
+tasklist | findstr /i autobooter.exe >nul 2>&1
+if errorlevel 1 (
+    
+    goto atbtr2
+) else (
+    
+	"%koshertek%\adb\fastboot.exe" reboot >nul 2>&1
+)
+
+
+"%koshertek%\adb\fastboot.exe" --disable-verity --disable-verification flash recovery "%koshertek%\4058R\finalrec.img" >nul 2>&1
+
+
+TASKKILL /F /IM autobooter.exe >nul 2>&1
+"%koshertek%\adb\fastboot.exe" reboot >nul 2>&1
+echo.
+cls
+color 5f
+echo ##############################################
+echo              KosherTek Flash Tool             
+echo ##############################################
+echo.
+echo Flashing complete. You may now disconnect the phone. Phone will now restart... 
+echo.
+echo Press enter to exit.
+pause >nul 2>&1
+color
+endlocal
+    
+    
